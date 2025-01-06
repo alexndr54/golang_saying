7185000d970ea5aba059b483cf54116de0b13def
Alex <alexndr544@gmail.com>
2025-01-06 23:50:33 +0700

    fixing

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/alexndr54/golang_saying
+module github.com/alexndr54/golang_saying/v2
 
 go 1.23.4
--- saying.go
+++ saying.go
@@ -2,7 +2,9 @@ package golang_saying
 
 import "fmt"
 
-func Saying(saying string) {
+func Saying(saying string, isAdmin bool) {
 	fmt.Println("Hai kak,Hayo mau ngomong apa")
 	fmt.Println(saying)
+
+	fmt.Println(Filter(saying))
 }
