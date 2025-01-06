97f905b727ddccd48618a48225fad6a0e38a5585
Alex <alexndr544@gmail.com>
2025-01-06 23:13:44 +0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alexndr54/golang_saying
+
+go 1.23.4
---
+++ saying.go
@@ -0,0 +1,8 @@
+package golang_saying
+
+import "fmt"
+
+func saying(saying string) {
+	fmt.Println("Hai kak,Hayo mau ngomong apa")
+	fmt.Println(saying)
+}
